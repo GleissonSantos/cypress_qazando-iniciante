43105ef15def50844cf0a4ce92d22933c5ce106f
Gleisson  Rodrigo <gleissonrodrigo@MacBook-Pro-de-Gleisson.local>
2025-09-18 22:42:25 -0300

    alterando arquivo de cadastro usuario

--- cypress/e2e/cadastro_usuario.cy.js
+++ cypress/e2e/cadastro_usuario.cy.js
@@ -6,26 +6,29 @@ import home_page from '../support/pages/home_page'
 
 describe('Cadastro de usuário', () => {
 
+        const name =  faker.person.fullName()
+        const email = faker.internet.email()
+
     beforeEach('Acessar cadastro de usuário', () => {
         commum_page.acessarCadastroUsuario()
     })
 
     it('Campo Nome vazio', () => {
-        
+
         cadastro_usuario_page.clicarBtnCadastrar()
         cadastro_usuario_page.checkErrormessage('O campo nome deve ser prenchido')
 
     })
 
     it('Campo E-mail vazio', () => {
-        cadastro_usuario_page.fillNamefield(faker.person.fullName())
+        cadastro_usuario_page.fillNamefield(name)
         cadastro_usuario_page.clicarBtnCadastrar()
         cadastro_usuario_page.checkErrormessage('O campo e-mail deve ser prenchido corretamente')
 
     })
 
     it('Campo E-mail inválido', () => {
-        cadastro_usuario_page.fillNamefield(faker.person.fullName())
+        cadastro_usuario_page.fillNamefield(name)
         cadastro_usuario_page.fillemailfield('Teste')
         cadastro_usuario_page.clicarBtnCadastrar()
         cadastro_usuario_page.checkErrormessage('O campo e-mail deve ser prenchido corretamente')
@@ -34,8 +37,8 @@ describe('Cadastro de usuário', () => {
 
     it('Campo senha vazia', () => {
 
-        cadastro_usuario_page.fillNamefield(faker.person.fullName())
-        cadastro_usuario_page.fillemailfield(faker.internet.email())
+        cadastro_usuario_page.fillNamefield(name)
+        cadastro_usuario_page.fillemailfield(email)
         cadastro_usuario_page.clicarBtnCadastrar()
         cadastro_usuario_page.checkErrormessage('O campo senha deve ter pelo menos 6 dígitos')
 
@@ -43,8 +46,8 @@ describe('Cadastro de usuário', () => {
 
     it('Campo senha inválida', () => {
 
-        cadastro_usuario_page.fillNamefield(faker.person.fullName())
-        cadastro_usuario_page.fillemailfield(faker.internet.email())
+        cadastro_usuario_page.fillNamefield(name)
+        cadastro_usuario_page.fillemailfield(email)
         cadastro_usuario_page.fillpasswordlfield('123')
         cadastro_usuario_page.clicarBtnCadastrar()
         cadastro_usuario_page.checkErrormessage('O campo senha deve ter pelo menos 6 dígitos')
@@ -53,11 +56,9 @@ describe('Cadastro de usuário', () => {
 
     // Função 'async', o await obriga o codigo esperar rodar o fake antes de passar para as proximas linhas
     it('Cadastro com sucesso', async () => {
-        
-        const name = await faker.person.fullName()
 
         cadastro_usuario_page.fillNamefield(name)
-        cadastro_usuario_page.fillemailfield(faker.internet.email())
+        cadastro_usuario_page.fillemailfield(email)
         cadastro_usuario_page.fillpasswordlfield('123456')
         cadastro_usuario_page.clicarBtnCadastrar()
         home_page.checkRegistersucess(name)
